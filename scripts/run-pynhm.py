2e9527a8223e4e05870cb810bd75d4f56b5ce2bd
Mike Fienen <mnfienen@usgs.gov>
2023-01-19 08:41:36 -0600

    moving run script to scripts directory

---
+++ scripts/run-pynhm.py
@@ -0,0 +1,30 @@
+import pathlib as pl
+import time
+import pynhm
+
+
+work_dir = pl.Path("./")
+out_dir = work_dir / "output"
+out_dir.mkdir(parents=True, exist_ok=True)
+
+params = pynhm.PrmsParameters.load(work_dir / "myparam.param")  # this line is necessary to avoid error in channel graph construction. apparently the parameters are edited. should probably copy them
+control = pynhm.Control.load(work_dir / "control.test", params=params)
+
+multi_proc_model = pynhm.Model(
+    pynhm.PRMSSolarGeometry,
+    pynhm.PRMSAtmosphere,
+    pynhm.PRMSCanopy,
+    pynhm.PRMSSnow,
+    pynhm.PRMSRunoff,
+    pynhm.PRMSSoilzone,
+    pynhm.PRMSGroundwater,
+    pynhm.PRMSChannel,
+    control=control,
+    input_dir=work_dir,
+    budget_type=None,
+)
+
+sttime = time.time()
+multi_proc_model.run(netcdf_dir=out_dir, finalize=True)
+print(f'That took {time.time()-sttime:.3f} looong seconds')
+
