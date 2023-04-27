f35bb05df32cb980b007d47c6430e0ca43e26947
Joseph Hughes <jdhughes@usgs.gov>
2023-04-27 11:41:00 -0500

    update for pynhm to pywatershed renaming

--- NHM_extractions/20230110_pois_haj/01473000/run-pynhm.py
+++ NHM_extractions/20230110_pois_haj/01473000/run-pynhm.py
@@ -1,28 +1,28 @@
 import pathlib as pl
 import time
-import pynhm
+import pywatershed
 
 
 work_dir = pl.Path("./")
 out_dir = work_dir / "output"
 out_dir.mkdir(parents=True, exist_ok=True)
 
-params = pynhm.PrmsParameters.load(work_dir / "myparam.param")  # this line is necessary to avoid error in channel graph construction. apparently the parameters are edited. should probably copy them
-control = pynhm.Control.load(work_dir / "control.test", params=params)
+params = pywatershed.parameters.PrmsParameters.load(work_dir / "myparam.param")  # this line is necessary to avoid error in channel graph construction. apparently the parameters are edited. should probably copy them
+control = pywatershed.Control.load(work_dir / "control.test", params=params)
 
-multi_proc_model = pynhm.Model(
-    pynhm.PRMSSolarGeometry,
-    pynhm.PRMSAtmosphere,
-    pynhm.PRMSCanopy,
-    pynhm.PRMSSnow,
-    pynhm.PRMSRunoff,
-    pynhm.PRMSSoilzone,
-    pynhm.PRMSGroundwater,
-    pynhm.PRMSChannel,
+multi_proc_model = pywatershed.Model(
+    pywatershed.PRMSSolarGeometry,
+    pywatershed.PRMSAtmosphere,
+    pywatershed.PRMSCanopy,
+    pywatershed.PRMSSnow,
+    pywatershed.PRMSRunoff,
+    pywatershed.PRMSSoilzone,
+    pywatershed.PRMSGroundwater,
+    pywatershed.PRMSChannel,
     control=control,
     input_dir=work_dir,
     budget_type=None,
-    verbose=True,
+    verbose=False,
     calc_method='numba',
 )
 multi_proc_model.initialize_netcdf(
