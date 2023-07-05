3ba23fba58a24e6d321e5366ed0a60550ed8d235
ahaj <ahaj@usgs.gov>
2023-07-05 16:28:21 -0500

    Plugged away a little on grouping in Pest++ setup notebook.

--- scripts/run-pynhm.py
+++ scripts/run-pynhm.py
@@ -1,31 +1,36 @@
 import pathlib as pl
 import time
-import pynhm
+import pywatershed
 
 
 work_dir = pl.Path("./")
 out_dir = work_dir / "output"
 out_dir.mkdir(parents=True, exist_ok=True)
 
-params = pynhm.PrmsParameters.load(work_dir / "myparam.param")  # this line is necessary to avoid error in channel graph construction. apparently the parameters are edited. should probably copy them
-control = pynhm.Control.load(work_dir / "control.test", params=params)
+params = pywatershed.parameters.PrmsParameters.load(work_dir / "parameters.json")  # this line is necessary to avoid error in channel graph construction. apparently the parameters are edited. should probably copy them
+control = pywatershed.Control.load(work_dir / "control.test", params=params)
 
-multi_proc_model = pynhm.Model(
-    pynhm.PRMSSolarGeometry,
-    pynhm.PRMSAtmosphere,
-    pynhm.PRMSCanopy,
-    pynhm.PRMSSnow,
-    pynhm.PRMSRunoff,
-    pynhm.PRMSSoilzone,
-    pynhm.PRMSGroundwater,
-    pynhm.PRMSChannel,
+multi_proc_model = pywatershed.Model(
+    pywatershed.PRMSSolarGeometry,
+    pywatershed.PRMSAtmosphere,
+    pywatershed.PRMSCanopy,
+    pywatershed.PRMSSnow,
+    pywatershed.PRMSRunoff,
+    pywatershed.PRMSSoilzone,
+    pywatershed.PRMSGroundwater,
+    pywatershed.PRMSChannel,
     control=control,
     input_dir=work_dir,
     budget_type=None,
-    calc_method = 'numba'
+    verbose=False,
+    calc_method='numba',
+)
+multi_proc_model.initialize_netcdf(
+    output_dir=out_dir,
+    separate_files=True,
 )
 
 sttime = time.time()
-multi_proc_model.run(netcdf_dir=out_dir, finalize=True)
+multi_proc_model.run(finalize=True)
 print(f'That took {time.time()-sttime:.3f} looong seconds')
 
