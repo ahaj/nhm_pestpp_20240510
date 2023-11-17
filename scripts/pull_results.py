13ffa96ef7d7d92924d7c1e092582d547ac5c5e6
Mike Fienen <mnfienen@usgs.gov>
2023-11-17 14:19:51 -0600

    added pull_results.py: allows programmatic pulling of zipfiles from the open storage pod

---
+++ scripts/pull_results.py
@@ -0,0 +1,9 @@
+import pathlib as pl
+import zipfile
+import os
+
+all_extractions = ['01473000','05431486','09112500']
+rootnm = 'prior_mc_reweight'
+
+for cex in all_extractions:
+    os.system(f"aws s3 cp s3://hytest-workspace/mnfienen/{rootnm}.{cex}.zip ../results/{rootnm}.{cex}.zip --endpoint-url https://usgs.osn.mghpcc.org/ --profile osn-hytest-workspace")
