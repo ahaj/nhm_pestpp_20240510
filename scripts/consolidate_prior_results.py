923cba9ada756fb8db3229d6e94dfe4279ab6f84
Mike Fienen <mnfienen@usgs.gov>
2023-11-17 20:13:13 +0000

    added pushing files to storage to consolidate_results.py

--- scripts/consolidate_prior_results.py
+++ scripts/consolidate_prior_results.py
@@ -1,5 +1,6 @@
 import pathlib as pl
 import zipfile
+import os
 
 all_extractions = ['01473000','05431486','09112500']
 rootnm = 'prior_mc_reweight'
@@ -11,5 +12,8 @@ for cex in all_extractions:
     files.extend(cpath.glob(f'{rootnm}*pdc*'))
     files.extend(cpath.glob(f'{rootnm}*phi*'))
     files.extend(cpath.glob(f'{rootnm}.0*'))
+    files.extend(cpath.glob(f'{rootnm}.*noise*'))
     with zipfile.ZipFile(results_dir / f'{rootnm}.{cex}.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
         [zipf.write(i, i.name) for i in files]
+    pt = str(results_dir / f'{rootnm}.{cex}.zip')
+    os.system(f"aws s3 cp {pt} s3://hytest-workspace/mnfienen/{rootnm}.{cex}.zip --endpoint-url https://usgs.osn.mghpcc.org/ --profile osn-hytest-workspace")
