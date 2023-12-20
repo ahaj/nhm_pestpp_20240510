d458a42b7b2d1fcc830888815f159864c33b2718
Mike Fienen <mnfienen@usgs.gov>
2023-12-20 21:12:19 +0000

    minor script changes for pre/post processing

--- scripts/prep_condor_runs.py
+++ scripts/prep_condor_runs.py
@@ -12,6 +12,7 @@ for cex in all_extractions:
 
     files = [i for i in list(frompath.glob(f'{rootnm}*') ) if 'phi' not in i.name]
     files.extend([i for i in list(frompath.glob(f'*.tpl') ) ]) 
+    files.extend([i for i in list(frompath.glob(f'*.py') ) ])
 
     for tf in tofolder:
         [shutil.copy2(f, topath / tf / f.name) for f in files]
