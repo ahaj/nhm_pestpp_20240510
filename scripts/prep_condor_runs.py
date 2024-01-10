d2f9fbc3d0130eae4c7995ceef31a6da3a0a4037
Mike Fienen <mnfienen@usgs.gov>
2024-01-10 18:39:23 +0000

    adding ins to prep runs script copies

--- scripts/prep_condor_runs.py
+++ scripts/prep_condor_runs.py
@@ -13,6 +13,7 @@ for cex in all_extractions:
     files = [i for i in list(frompath.glob(f'{rootnm}*') ) if 'phi' not in i.name]
     files.extend([i for i in list(frompath.glob(f'*.tpl') ) ]) 
     files.extend([i for i in list(frompath.glob(f'*.py') ) ])
+    files.extend([i for i in list(frompath.glob(f'*.ins') ) ])
     files.append(frompath / 'loc.mat')
 
     for tf in tofolder:
