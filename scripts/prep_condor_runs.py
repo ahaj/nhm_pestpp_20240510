0dfd72d3d1ebcdade2b71814270fe4326195f368
Mike Fienen <mnfienen@usgs.gov>
2024-01-17 02:38:31 +0000

    updated prep_condor_runs.py

--- scripts/prep_condor_runs.py
+++ scripts/prep_condor_runs.py
@@ -1,10 +1,10 @@
 import pathlib as pl
 import shutil
 
-all_extractions = ['01473000','05431486','09112500']
+all_extractions = ['01473000']#,'05431486','09112500']
 rootnm = 'prior_mc_reweight'
 results_dir = pl.Path('../results')
-
+hotstart = True
 for cex in all_extractions:
     frompath = pl.Path(f'../NHM_extractions/20230110_pois_haj/{cex}/')
     topath = pl.Path(f'../../CONDOR/{cex}')
@@ -14,6 +14,8 @@ for cex in all_extractions:
     files.extend([i for i in list(frompath.glob(f'*.tpl') ) ]) 
     files.extend([i for i in list(frompath.glob(f'*.py') ) ])
     files.extend([i for i in list(frompath.glob(f'*.ins') ) ])
+    if hotstart:
+        files.extend([i for i in list(frompath.glob(f'*hot*'))])
     files.append(frompath / 'loc.mat')
 
     for tf in tofolder:
