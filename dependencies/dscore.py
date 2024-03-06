ad325c71c3a0de75cc83377547b1290467cd8e21
thodson <thodson@usgs.gov>
2024-03-06 14:47:58 -0600

    Minimal dscore demo

--- dependencies/dscore.py
+++ dependencies/dscore.py
@@ -9,8 +9,16 @@ def mse(obs, sim=0) -> float:
         .. math::
             \sum_{i=1}^{n}(x_i - \hat{x}_i)^2
 
-    :return: mean square error
-    :rtype: float
+    Parameters
+    ----------
+    obs : array_like
+        Observed values
+    sim : array_like
+        Simulated values
+
+    Returns
+    -------
+    float : mean squared error
 
     NOTE: this and all functions below rely upon the obs and sim datatypes implementing
           certain math methods on themselves.  That is, obs.sum() must be defined by
@@ -21,16 +29,117 @@ def mse(obs, sim=0) -> float:
     return pd.Series([np.array(e**2).mean()], index=['MSE'])
 
 
+def bias_distribution_sequence(obs, sim, objective=mse):
+    """Bias-distribution-sequence decomposition
+
+    WARNING: This works for MSE but hasn't been generalized to other objective functions.
+
+    Parameters
+    ----------
+    obs : array_like
+        Observed values
+    sim : array_like
+        Simulated values
+    objective : function
+        Error function, e.g. mse, etc.
+
+    Returns
+    -------
+    Series with entry for each component.
+
+    References
+    ----------
+    .. [1] Hodson et al., 2021. Mean squared error, deconstructed.
+    Journal of Advances in Earth Systems Modeling.
+    """
+    e = obs - sim
+    s = np.sort(obs) - np.sort(sim)
+    var_s = s.var()
+    var_e = e.var()
+
+    e_bias = objective(e)
+    e_dist = var_s
+    e_seq = var_e - var_s
+    names = ['bias', 'distribution', 'sequence']
+    return pd.Series([e_bias, e_dist, e_seq], index=names)
+
+
+def bias_variability(obs, sim, objective=mse, additive=False):
+    """
+    Bias-variability decomposition.
+
+    For MSE, this is equivalent to the classic bias-variance decomp.
+
+    Parameters
+    ----------
+    obs : array_like
+    sim : array_like
+    objective : function
+
+    Returns
+    -------
+    Series with entry for each component.
+
+    """
+    labels = ['bias', 'variability']
+    n_components = len(labels)
+
+    e = sim - obs
+    deviations = e - e.mean()
+
+    # mean is a hack to drop the labels and return a scalar
+    bias = objective(e - deviations).mean()
+    variability = objective(deviations).mean()
+
+    result = pd.Series([bias, variability], index=labels)
+
+    if not additive:
+        result = result * n_components
+
+    return result
+
+
+def seasons(obs, sim, objective=mse):
+    """Decompose error by season.
+
+    Parameters
+    ----------
+    obs : array_like
+    sim : array_like
+    objective : function
+
+    Returns
+    -------
+    Series with entry for each component.
+
+    """
+
+    def season(e, index):
+        return objective(e * index)
+
+    labels = ['winter', 'spring', 'summer', 'fall']
+    e = sim - obs
+
+    winter = season(e, (e.index.month == 12) | (e.index.month <= 2))
+    spring = season(e, (e.index.month > 2) & (e.index.month <= 5))
+    summer = season(e, (e.index.month > 5) & (e.index.month <= 8))
+    fall = season(e, (e.index.month > 8) & (e.index.month <= 11))
+
+    return pd.Series([winter, spring, summer, fall], index=labels)
+
+
 def quantiles(obs, sim, objective=mse, additive=False):
     """
     Decomposes MSE by quantile rangess 0.00-0.25; 0.25-0.5; 0.5-0.75; 0.75-1.00
 
-    Args:
+    Parameters
+    ----------
         obs (pd.Series - like  ): series of observed values
         sim (_type_): series of simulated/modeled values
     Both share a common index
 
-    Returns:
+    Returns
+    -------
         pd.Series : decomposed MSE, one value per quantile range
     """
     breaks = [0, 0.25, 0.5, 0.75, 1]
@@ -57,6 +166,8 @@ try:
 
     _SEASONAL = True
 except ImportError:
+    import logging
+
     logging.debug("STL library not available.")
     _SEASONAL = False
 
@@ -68,6 +179,18 @@ def stl(obs, sim, period=365, objective=mse):
     Seasonal and trend decomposition using Loess (STL).
     Note that STL is not perfectly orthogonal.
 
+    Parameters
+    ----------
+    obs : array_like
+    sim : array_like
+    period : int
+        Length of seasonal component.
+    objective : function
+
+    Returns
+    -------
+    Series with entry for each component.
+
     References
     ----------
     .. [1] Cleveland et al., 1990, STL: A seasonal-trend decomposition
@@ -76,6 +199,7 @@ def stl(obs, sim, period=365, objective=mse):
     if not _SEASONAL:
         logging.warning("STL statistics not available.")
         return None
+
     e = sim - obs
     bias = e.mean()
     res = STL(e, period=period, seasonal=9).fit()
@@ -128,20 +252,21 @@ def se(sigma_2, sd=1.96):
 
 
 # Plotting
+# Plotting
 def scorecard_plot(df, ax=None, clim=(0, 100), cmap='RdYlBu'):
     axs = ax or plt.gca()
 
     axs.set_ylabel("Component")
     # axs.set_yticks(np.arange(len(df.index)), labels=df.index, fontsize=6)
     axs.set_yticks(
-        np.arange(len(df.index)), labels=df.index.get_level_values(1), fontsize=9
+        np.arange(len(df.index)), labels=df.index.get_level_values(1), fontsize=6
     )
     axs.yaxis.set_tick_params(length=0, which='minor')
 
     axs.set_xticks(
         np.arange(len(df.columns)),
         labels=df.columns,
-        fontsize=9,
+        fontsize=6,
         rotation=90,
         ha='center',
         va='bottom',
