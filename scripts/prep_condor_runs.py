8ff42fc0ef0f5528a314d6cdf9e23c8e8738a7cf
EC2 Default User <ec2-user@ip-172-16-202-229.us-west-2.compute.internal>
2024-03-18 21:06:16 +0000

    Moved condor files to GENERAL and updated prep_condor)runs.py to populate from nothing

--- scripts/prep_condor_runs.py
+++ scripts/prep_condor_runs.py
@@ -5,18 +5,29 @@ all_extractions = ['01473000','05431486','09112500']
 rootnm = 'prior_mc_reweight'
 results_dir = pl.Path('../results')
 hotstart = False
+first_time=True
 for cex in all_extractions:
     frompath = pl.Path(f'../NHM_extractions/20230110_pois_haj/{cex}/')
-    topath = pl.Path(f'../../CONDOR/{cex}')
+    topath = pl.Path(f'../CONDOR/{cex}')
+    genpath = pl.Path('../CONDOR/GENERAL')
     tofolder = ['MASTER','data']
+    if first_time:
+        if not topath.exists():
+            topath.mkdir()
+        if not (topath / 'log').exists():
+            (topath / 'log').mkdir()
+        [shutil.copy2(f, topath / f.name) for f in genpath.glob('*')]
+        for tf in tofolder:
+            print(f'copying {frompath} to CONDOR rundir')
+            shutil.copytree(frompath, topath/tf, dirs_exist_ok=True)
+    else:
+        files = [i for i in list(frompath.glob(f'{rootnm}*') ) if 'phi' not in i.name]
+        files.extend([i for i in list(frompath.glob(f'*.tpl') ) ]) 
+        files.extend([i for i in list(frompath.glob(f'*.py') ) ])
+        files.extend([i for i in list(frompath.glob(f'*.ins') ) ])
+        if hotstart:
+            files.extend([i for i in list(frompath.glob(f'*hot*'))])
+        files.append(frompath / 'loc.mat')
 
-    files = [i for i in list(frompath.glob(f'{rootnm}*') ) if 'phi' not in i.name]
-    files.extend([i for i in list(frompath.glob(f'*.tpl') ) ]) 
-    files.extend([i for i in list(frompath.glob(f'*.py') ) ])
-    files.extend([i for i in list(frompath.glob(f'*.ins') ) ])
-    if hotstart:
-        files.extend([i for i in list(frompath.glob(f'*hot*'))])
-    files.append(frompath / 'loc.mat')
-
-    for tf in tofolder:
-        [shutil.copy2(f, topath / tf / f.name) for f in files]
+        for tf in tofolder:
+            [shutil.copy2(f, topath / tf / f.name) for f in files]
