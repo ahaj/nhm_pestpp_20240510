8d4e03d75c4cf06d447755b6755f62111e287c79
Mike Fienen <mnfienen@usgs.gov>
2024-03-18 23:06:49 +0000

    ready to pull hotstart files

--- scripts/prep_condor_runs.py
+++ scripts/prep_condor_runs.py
@@ -1,11 +1,11 @@
 import pathlib as pl
 import shutil
 
-all_extractions = ['01473000','05431486','09112500']
-rootnm = 'prior_mc_reweight'
+all_extractions = ['01473000']#,'05431486','09112500']
+rootnm ='ies_hot'# 'prior_mc_reweight'
 results_dir = pl.Path('../results')
-hotstart = False
-first_time=True
+hotstart = True
+first_time=False
 for cex in all_extractions:
     frompath = pl.Path(f'../NHM_extractions/20230110_pois_haj/{cex}/')
     topath = pl.Path(f'../CONDOR/{cex}')
